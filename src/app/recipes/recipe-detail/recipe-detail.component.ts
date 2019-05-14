f7f986ef7cac981edc58d9f79660a22c168da6ed
Nikesh Pandya <33998764+nikesh9220@users.noreply.github.com>
2019-05-14 17:13:41 +0530

    Project Structure

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,6 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html"
+})
+export class RecipeDetailComponent {}
